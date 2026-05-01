632f4f5cea1992f5d01af9b7772a913e20c3906f
Aljon <aljontroshani6@gmail.com>
2026-05-01 19:57:36 +0200

    shtova productcard,ndryshova stilizimin dhe bera lidhjen midis te dhenave dhe productcard

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+function ProductCard({ name, price, image }) {
+  return (
+    <div className="card">
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
+      <p>${price}</p>
+      <button>Add to Cart</button>
+    </div>
+  );
+}
+
+export default ProductCard;
