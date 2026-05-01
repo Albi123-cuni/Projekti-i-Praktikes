089d1997e37a85f31e96aae43f35b1c04688fc06
Albi123-cuni <proalbi076@gmail.com>
2026-05-01 21:29:34 +0200

    save

--- src/counter.jsx
+++ src/counter.jsx
@@ -1,7 +1,7 @@
-import React from 'react';
+
 import { useState } from 'react';
 
-function Counter() {
+export default function Counter() {
     const [count, setCount] = useState(0);
 
     const increment = () => {
